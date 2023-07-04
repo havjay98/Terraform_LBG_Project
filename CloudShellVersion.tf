fdf9db501cabf882c56ecbf423643e85696be391
ZaphnathPaaneah11 <137209658+ZaphnathPaaneah11@users.noreply.github.com>
2023-07-04 16:52:00 +0100

    Update CloudShellVersion.tf

--- CloudShellVersion.tf
+++ CloudShellVersion.tf
@@ -1,4 +1,31 @@
-resource "google_project" "my_project" {  
-  name          = "my-lloyds-challenge-project"
-  project_id    = "my-lloyds-challenge-project-id"
+# main.tf
+
+ 
+
+# Configure the Google Cloud provider
+provider "google" {
+ project     = ""
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
+
+ 
+
+# Create a Google Cloud Storage bucket
+resource "google_storage_bucket" "private_bucket" {
+  name          = ""
+  location      = "us-central1"
+  storage_class = "STANDARD"
+
+ 
+
+  lifecycle_rule {
+    condition {
+      age = 30 # Retain objects for 30 days
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
