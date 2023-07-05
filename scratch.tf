b8579dc0c1ce68b348cb31a6e2392b00709c079c
ZaphnathPaaneah11 <137209658+ZaphnathPaaneah11@users.noreply.github.com>
2023-07-05 16:33:48 +0100

    Update scratch.tf

--- scratch.tf
+++ scratch.tf
@@ -10,12 +10,26 @@ resource "google_storage_bucket" "private" {
 uniform_bucket_level_access = true   #object access determined by bucket permissions.
   
 retention_policy {
-retention_period = 3000000 # objects in the bucket can only be deleted or replaced once the age is greater than 3000000 seconds.
+	retention_period = 3000000 # objects in the bucket can only be deleted or replaced once the age is greater than 3000000 seconds.
   }
 }
 
 
+
+
 # A user managed vertex AI Notebook
+# All environments use JupyterLab 3 by default and have latest Nvidia GPU and Intel libraries and drivers installed.
+
+resource "google_notebooks_instance" "basic" {
+ 	name = ""
+  	region = "us-central1"
+	zone = "us-central1-a"
+  	machine_type = "e2-medium"
+  vm_image {
+    	project      = ""
+    	image_family = "tf-latest-cpu"
+  	}
+}
 
 
  
