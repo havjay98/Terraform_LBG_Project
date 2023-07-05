53faf78792ba62b4f21a796fe9dbb5ba71131a0b
ZaphnathPaaneah11 <137209658+ZaphnathPaaneah11@users.noreply.github.com>
2023-07-05 13:24:48 +0100

    Update scratch.tf

--- scratch.tf
+++ scratch.tf
@@ -1,15 +1,18 @@
 # A private Google cloud storage bucket with a retention policy
 
 resource "google_storage_bucket" "private" {
- name          = ""
- location      = "US"
- storage_class = "STANDARD"
+	count = 1
+	name = ""
+	public_access_prevention = "enforced"
+location = "US" 
+	storage_class = "STANDARD"
 
- uniform_bucket_level_access = true   #object access determined by bucket permissions.
+uniform_bucket_level_access = true   #object access determined by bucket permissions.
   
 retention_policy {
-  retention_period = 3000000 # objects in the bucket can only be deleted or replaced once the age is greater than 3000000 seconds.
+retention_period = 3000000 # objects in the bucket can only be deleted or replaced once the age is greater than 3000000 seconds.
   }
 }
+
 
  
