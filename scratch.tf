e9fc5b1cbd59842c266e3ec95c81f05057db33e8
ZaphnathPaaneah11 <137209658+ZaphnathPaaneah11@users.noreply.github.com>
2023-07-06 12:05:32 +0100

    Update scratch.tf

--- scratch.tf
+++ scratch.tf
@@ -35,7 +35,7 @@ resource "google_notebooks_instance" "basic" {
  
 # A BigQuery dataset (empty or with sample data) which includes a configured optimisation that could speed up queries
 
-resource "google_bigquery_dataset" "empyt_dataset" {
+resource "google_bigquery_dataset" "empty_dataset" {
   dataset_id = "<DATASET_ID>"
   project   = ""
 
